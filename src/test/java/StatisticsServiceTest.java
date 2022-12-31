925167c9f1a69e73957dbf1c73f74d707859f090
DENIVON95 <ivonin.denis@mail.ru>
2022-12-31 13:58:38 +0400

    added test

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -19,7 +19,7 @@ public class StatisticsServiceTest {
     void findMaxInMiddle() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 50, 3, 8, 6, 11, 11, 12};
         long expected = 50;
 
         long actual = service.findMax(incomesInBillions);
